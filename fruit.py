94e80839e1a60a4a4e23f5443c8668ec1f6477c1
1415475570 <1415475570@qq.com>
2021-01-19 20:23:57 +0800

    0

---
+++ fruit.py
@@ -0,0 +1,73 @@
+name1="苹果"
+price1=10
+num1=5
+unit1="个"
+
+name2="香蕉"
+price2=15
+num2=10
+unit2="个"
+
+name3="菠萝"
+price3=20
+num3=1
+unit3="个"
+
+name4="西瓜"
+price4=25
+num4=1
+unit4="个"
+
+print("---------------------水果商城---------------------")
+print("名称"+"           "+"单价"+"           "+"重量"+"           "+"数量")
+print("------------------------------------------------")
+print(name1+"           ",price1,"            ",num1,"            ",unit1)
+print(name2+"           ",price2,"            ",num2,"            ",unit2)
+print(name3+"           ",price3,"            ",num3,"            ",unit3)
+print(name4+"           ",price4,"            ",num4,"            ",unit4)
+print("------------------------------------------------")
+print("总价：",(price1*num1+price2*num2+price3*num3+price4*num4))
+
+
+
+
+brand1="优衣库"
+quality1="S"
+price1=999.0
+num1=1
+unit1="件"
+
+brand2="耐克"
+quality2="A"
+price2=998.0
+num2=1
+unit2="件"
+
+brand3="阿迪达斯"
+quality3="S"
+price3=599.0
+num3=1
+unit3="件"
+
+brand4="李宁"
+quality4="S"
+price4=299.0
+num4=1
+unit4="双"
+
+brand5="美特斯邦威"
+quality5="S"
+price5=499.0
+num5=1
+unit5="个"
+
+print("-----------------------水果商城-----------------------")
+print("名称"+"           "+"品质"+"           "+"单价"+"        "+"数量"+"       "+"单位")
+print("-----------------------------------------------------")
+print(brand1+"          ",quality1,"         ",price1,"      ",num1,"       ",unit1)
+print(brand2+"            ",quality2,"         ",price2,"      ",num2,"       ",unit2)
+print(brand3+"         ",quality3,"         ",price3,"      ",num3,"       ",unit3)
+print(brand4+"            ",quality4,"         ",price4,"      ",num4,"       ",unit4)
+print(brand5+"       ",quality5,"         ",price5,"      ",num5,"       ",unit5)
+print("-----------------------------------------------------")
+print("总价：",(price1*num1+price2*num2+price3*num3+price4*num4+price5*num5))
